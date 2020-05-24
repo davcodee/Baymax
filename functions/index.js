2628c6196a5200c891e697ca84c3d43be7d7df5b
David Morales <david_ivan2402@ciencias.unam.mx>
2020-05-23 23:58:56 -0500

    se agregaron los intents faltantes

--- functions/index.js
+++ functions/index.js
@@ -1,48 +1,108 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
 // See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
 // for Dialogflow fulfillment library docs, samples, and to report issues
 'use strict';
- 
-const functions = require('firebase-functions');
+
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
- 
+
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
- 
+
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
 
-  
-  const agent = new WebhookClient({ request, response });
-  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
+    const agent = new WebhookClient({ request, response });
+    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+    /*tiempo no elegido por el usuario*/
+    var error_tiempo = false;
+    /*Tiempo verbal que selecciono el usuario*/
+    var tiempo_verbal ="";
     // Ejercicios simple present
     var exercise_present = [['she _____ (not/work)in a hospital', "doesn't works",'does not works' ],['she ___(work) in a hospital', 'works']
-                            ['___ she ____(work) play basketball?', "doesn't works",'does not works' ], []];
-    // Ejercicios simple future
-    var exercise_future  = [];
-    // Ejercicios simple past
-    var exercise_past = [];
+        ['___ she ____(work) play basketball?', "doesn't works",'does not works' ]];
+    
+    
+    function repasar(agent) {
+        var t = agent.parameters.tiempo;
+        if(t  === "Presente"){
+            tiempo_verbal = "Presente";
+            agent.add(`Vamos a repasar el presente simple`);
+            // TODO: mostrar el ejercicio del presente simple
+            //TODO : avisar a repasar oracion que ejerecicio fue
 
-  /**
-   * Funcion que maneja el intent repasar
-   * @param: agent
-   * return:
-   * */
-  function repasar(agent) {
-      agent.add('Muy bien vamos a prácticar');
-      /*verificamos el tipo de tiempo y activamos la bandera*/
-        if(agent.parameters.tiempo == "Presente"){
-            agent.add("Vale, practicaremos el presente simple");
-        } else if (agent.parameters.tiempo =="Pasado"){
-            agent.add("Vale, practicaremos el pasado simple");
-        }else if(agent.parameters.tiempo == "Futuro"){
-            agent.add("Vale, practicaremos el futuro simple");
-        }else{
-            agent.add('¿Qué tiempo te gustaría repasar ?')
+        } if (t=== "Futuro"){
+            tiempo_verbal = "Futuro";
+            agent.add(`Vamos a repasar el futuro simple`);
+            // TODO: mostrar el ejercicio del futuro simple
+            //TODO : avisar a repasar oracion que ejerecicio fue
+
+        } if (t === "Pasado"){
+            tiempo_verbal = "Pasado";
+            agent.add(`Vamos a repasar el pasado simple`);
+            // TODO: mostrar el ejercicio del pasado simple
+            //TODO : avisar a repasar oracion que ejerecicio fue
+
+        }else {
+            agent.add("¿Que tiempo te gustaría repasar?");
+            error_tiempo = true;
         }
+    }
 
-  }
-  
+
+    function repasartiempo(agent){
+        /*Si el usuario no selecciono el tiempo verbal*/
+        if(error_tiempo = true){
+            /*seleccionamos el tiempo verbal*/
+            var t = agent.parameters.tiempo;
+
+            if(t  === "Presente"){
+                agent.add(`Vamos a repasar el presente simple`);
+                agent.add("La siguiente oración  está en simple present, completala:");
+                agent.add(exercise_present[0][0]);
+                // TODO: mostrar el ejercicio del presente simple
+                //TODO : avisar a repasar oracion que ejerecicio fue
+            } if (t=== "Futuro"){
+                agent.add(`Vamos a repasar el futuro simple`);
+                agent.add("La siguiente oración  está en simple present, completala:");
+                agent.add(exercise_present[0][0]);
+                // TODO: mostrar el ejercicio del futuro simple
+                //TODO : avisar a repasar oracion que ejerecicio fue
+            } if (t === "Pasado"){
+                agent.add(`Vamos a repasar el pasado simple`);
+                agent.add("La siguiente oración  está en simple present, completala:");
+                agent.add(exercise_present[0][0]);
+                // TODO: mostrar el ejercicio del futuro simple
+                //TODO : avisar a repasar oracion que ejerecicio fue
+            }
+        }
+    }
+
+
+    function repasaroracion(agent) {
+        //TODO: checar que ejercicio es y hacer la revisión
+        if(tiempo_verbal  === "Presente"){
+            /*verificamos el ejercicio*/
+            agent.add(`ejercicio  presente simple`);
+            // TODO: mostrar  los siguientes 5 ejercicios
+        } if (tiempo_verbal === "Futuro"){
+            /*verificamos el ejercicio*/
+            agent.add(`ejercicio  futuro simple`);
+
+        } if (tiempo_verbal === "Pasado"){
+            /*verificamos el ejercicio*/
+            agent.add(`ejercicio pasado simple`);
+
+        }
+    }
 
     /**
      *  Funcion que maneja el intent aprender
@@ -50,17 +110,17 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
      *  @return
      * */
 
-  function aprender(agent){
-      var t = agent.parameters.tiempo;
+    function aprender(agent){
+        var t = agent.parameters.tiempo;
 
-      if(t === "Pasado" ){
-          agent.ask(`Here's an example of a browsing carousel.`);
-      } else if (t === "Futuro"){
-          /*Descripcion*/
-          agent.add( new Card({
-              title: 'Futuro simple',
-              imageUrl:'https://i.imgur.com/CdUvVRI.jpg',
-              text: `Para el futuro simple se utiliza will, éste tiempo se usa:\n
+        if(t === "Pasado" ){
+            agent.ask(`Here's an example of a browsing carousel.`);
+        } else if (t === "Futuro"){
+            /*Descripcion*/
+            agent.add( new Card({
+                title: 'Futuro simple',
+                imageUrl:'https://i.imgur.com/CdUvVRI.jpg',
+                text: `Para el futuro simple se utiliza will, éste tiempo se usa:\n
                     1. Con acciones voluntarias, por ejemplo:\n
                     They will clean their rooms. (Limpiarán sus habitaciones.)\n
                     2. Para expresar una promesa, por ejemplo:\n
@@ -68,23 +128,23 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
                     3. Para hacer predicciones, por ejemplo:\n
                     It won’t rain.(No va a llover.)
                     `,
-          }));
+            }));
 
 
 
-          /*Tablita */
+            /*Tablita */
 
-          /* Preguntar que quiere hacer repasar o aprender*/
-          agent.add("Ahora ya sabes más ingles \n ¿Te gustaría seguir aprendiendo o repasando?")
-          agent.add(new Suggestion("Me gustaría aprender"));
-          agent.add(new Suggestion("Me gustaría repasar"));
+            /* Preguntar que quiere hacer repasar o aprender*/
+            agent.add("Ahora ya sabes más ingles \n ¿Te gustaría seguir aprendiendo o repasando?")
+            agent.add(new Suggestion("Me gustaría aprender"));
+            agent.add(new Suggestion("Me gustaría repasar"));
 
-      } else if (t === "Presente"){
-          /*Descripción*/
-          agent.add( new Card({
-              title: 'Presente simple ',
-              imageUrl:'https://i.imgur.com/SNxtOLK.jpg',
-              text: `El presente simple se utiliza para:
+        } else if (t === "Presente"){
+            /*Descripción*/
+            agent.add( new Card({
+                title: 'Presente simple ',
+                imageUrl:'https://i.imgur.com/SNxtOLK.jpg',
+                text: `El presente simple se utiliza para:
                     1. Hablar de cosas que suceden habitualmente,\n por ejemplo:
                     He never eats vegetables. (Nunca come las verduras).\n
                     2. Para hablar de generalidades o hechos científicos,\n
@@ -94,31 +154,28 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
                     4. Para dar instrucciones (el imperativo).\n
                     Eat the vegetables.(Come las verduras).\n
                     `,
-          }));
+            }));
 
-        /* tablita*/
+            /* tablita*/
 
-        /* Preguntar que quiere hacer repasar o aprender*/
-        agent.add("Ahora ya sabes más ingles \n ¿Te gustaría seguir aprendiendo o practicando?")
+            /* Preguntar que quiere hacer repasar o aprender*/
+            agent.add("Ahora ya sabes más ingles \n ¿Te gustaría seguir aprendiendo o practicando?")
 
+        }else{
+            agent.add("¿Que tiempo te gustaría aprender?");
+            agent.add(new Suggestion("Pasado simple"));
+            agent.add(new Suggestion("Presente simple"));
+            agent.add(new Suggestion("Futuro simple"));
 
-      }else{
-          agent.add("¿Que tiempo te gustaría aprender?");
-          agent.add(new Suggestion("Pasado simple"));
-          agent.add(new Suggestion("Presente simple"));
-          agent.add(new Suggestion("Futuro simple"));
+        }
+    }
 
-      }
-  }
+    let intentMap = new Map();
 
-  let intentMap = new Map();
-  //intentMap.set('Obtener ciudad', obtenerCiudad);
-  //intentMap.set('Live', detallePlatziLive);
-  //intentMap.set('Talleres', seleccionDeTematica);
-  //intentMap.set('Seleccion de taller', detalleTaller);
-  //intentMap.set('Seleccion de taller - yes', registroAlTaller);
-  //intentMap.set('Talleres - Deep Links', selectDeepOption);
-  intentMap.set('aprender', aprender);
-  intentMap.set('repasar', repasar);
-  agent.handleRequest(intentMap);
+    intentMap.set('aprender', aprender);
+    intentMap.set('repasar', repasar);
+    intentMap.set('repasar-tiempo',repasartiempo);
+    intentMap.set('repasar-oracion',repasaroracion);
+    agent.handleRequest(intentMap);
 });
+
